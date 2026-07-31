7c5318f3b536735d1fe65b3ff84b02bb69b7d4fe
jinyiliang <3481845992@qq.com>
2026-07-31 18:02:07 +0800

    modify:String类的utf-8

--- src/main/java/org/maxing/learning/string/Main.java
+++ src/main/java/org/maxing/learning/string/Main.java
@@ -1,5 +1,7 @@
 package org.maxing.learning.string;
 
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args){
         String str="123123";
@@ -15,7 +17,7 @@ public class Main {
         String lower=str2.toLowerCase();
         System.out.println(lower);
 
-        String str3="ok,i will told him when i met her";
+        String str3="  ok,i will told him when i met her";
         String[]strs=str3.split(" ");
         for(String curStr:strs){
             System.out.println(curStr);
@@ -23,5 +25,19 @@ public class Main {
 
         String str4=String.join(" ",strs);
         System.out.println(str4);
+
+        str4=str4.strip();
+        System.out.println(str4);
+
+        byte[]bytes=str4.getBytes(StandardCharsets.UTF_8);
+        for(byte b:bytes){
+            System.out.print(b+" ");
+        }
+        System.out.println();
+        String str5=new String(bytes,StandardCharsets.UTF_8);
+        System.out.println(str5);
+
+        String str6=String.format("Name:%s, age:%d","Michael",19);
+        System.out.println(str6);
     }
 }
