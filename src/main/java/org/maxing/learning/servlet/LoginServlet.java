f830cb43e12e8925dd2f816b5102f4ffa154445e
jinyiliang <3481845992@qq.com>
2026-09-04 00:01:55 +0800

    feature(HttpServlet):新增自定义servlet,并且添加到Tomcat中 1 替换servlet为HttpServlet

--- src/main/java/org/maxing/learning/servlet/LoginServlet.java
+++ src/main/java/org/maxing/learning/servlet/LoginServlet.java
@@ -1,83 +1,36 @@
 package org.maxing.learning.servlet;
 
-import jakarta.servlet.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Enumeration;
 
-public class LoginServlet implements Servlet {
-    private ServletConfig config;
-
-    public LoginServlet() {
-        System.out.println("create servlet object");
-    }
-
+public class LoginServlet extends HttpServlet {
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        System.out.println("init login_servlet");
-        this.config=config;
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        System.out.println("getting login_servlet config");
-        return this.config;
-    }
-
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        System.out.println("================================");
-        System.out.println("start login_servlet service");
-
-        String encoding = req.getCharacterEncoding();
-        System.out.println("encoding is " + encoding);
-
-        Enumeration<String> attributeNames = req.getAttributeNames();
-        while (attributeNames.hasMoreElements()) {
-            String attributeName = attributeNames.nextElement();
-            Object attribute = req.getAttribute(attributeName);
-            System.out.println("attributeName is " + attributeName);
-            System.out.println("attribute is " + attribute);
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request head: ");
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = req.getHeader(headerName);
+            System.out.println(headerName +" : "+ headerValue);
         }
+        System.out.println("========================");
 
-        req.getParameterMap().forEach((key, value) -> {
-            System.out.println("key is " + key + " and value is " + Arrays.toString(value));
-        });
+        String contextPath = req.getContextPath();
+        System.out.println("contextPath: "+contextPath);
 
-        String protocol = req.getProtocol();
-        System.out.println("protocol is " + protocol);
+        String requestURI = req.getRequestURI();
+        System.out.println("requestURI: "+requestURI);
 
-        String serverName = req.getServerName();
-        System.out.println("serverName is " + serverName);
-        String serverPort = req.getServerPort() + "";
-        System.out.println("serverPort is " + serverPort);
-
-        System.out.println("================================");
-
-        res.setContentType("text/html;charset=utf-8");
-        String username=req.getParameter("username");
-        String password=req.getParameter("password");
-        if("admin".equals(username) && "Qwe123!@#".equals(password)){
-            req.setAttribute("username",username);
-            req.setAttribute("password",password);
-            PrintWriter writer = res.getWriter();
-            writer.println("<h2>login success</h2>");
-        }
-        else{
-            res.getWriter().println("<h2>login failed</h2>");
-        }
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "this is login_servlet info";
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("destroy login_servlet");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter respWriter = resp.getWriter();
+        respWriter.println("handle the request");
+        respWriter.flush();
+        respWriter.close();
     }
 }
