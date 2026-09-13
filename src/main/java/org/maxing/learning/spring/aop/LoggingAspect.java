d8b9eeb7c2be3e2bebc910f08c8d217723e38e92
jinyiliang <3481845992@qq.com>
2026-09-13 16:45:41 +0800

    feature(AOP):新增四种advice注解 1 LoggingAscpect新增四种advice注解，Before，AfterReturning，AfterThrowing，After。around没写案例

--- src/main/java/org/maxing/learning/spring/aop/LoggingAspect.java
+++ src/main/java/org/maxing/learning/spring/aop/LoggingAspect.java
@@ -1,9 +1,8 @@
 package org.maxing.learning.spring.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class LoggingAspect {
@@ -15,6 +14,27 @@ public class LoggingAspect {
 
     @Before("findStudentOperation()")
     public void before(JoinPoint joinPoint){
-        System.out.println("[AOP]:before method:"+joinPoint.getSignature().getName());
+        System.out.println("[AOP] before advice:"+joinPoint.getSignature());
+    }
+
+    @AfterReturning(
+            pointcut = "findStudentOperation()",
+            returning = "result"
+    )
+    public void afterReturning(JoinPoint joinPoint,Object result){
+        System.out.println("[AOP] after returning:"+joinPoint.getSignature().getName()+" execute success and return "+result);
+    }
+
+    @AfterThrowing(
+            pointcut="findStudentOperation()",
+            throwing = "ex"
+    )
+    public void afterThrowing(JoinPoint joinPoint,Throwable ex){
+        System.out.println("[AOP] after throwing:"+joinPoint.getSignature().getName()+" execute failed and throw "+ex);
+    }
+
+    @After("findStudentOperation()")
+    public void after(JoinPoint joinPoint){
+        System.out.println("[AOP] after advice:"+joinPoint.getSignature().getName()+" method execute finished");
     }
 }
